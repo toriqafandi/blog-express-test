7dd791b5aea30408f306e19526afe1569d166e0a
toriq <thoriqahmad17@gmail.com>
2023-04-19 13:28:06 +0700

    new

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+require('dotenv').config()
 const express = require('express')
-const { sequelize } = require('./models');
+const { sequelize, Product } = require('./models');
 const app = express()
 const port = process.env.PORT ?? 3000
 
@@ -7,8 +8,14 @@ app.get('/', (req, res) => {
     res.send('Hello World !')
 })
 
+app.get('/products', async (req, res) => {
+    const products = await Product.findAll();
+    res.json(products);
+})
+
 app.listen(port, async() => {
     console.log(`Service listen on port ${port}`)
+    console.log(JSON.stringify(process.env))
     try {
         await sequelize.authenticate();
         await sequelize.sync({ force: true});
